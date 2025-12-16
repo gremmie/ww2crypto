6a8c885c4c2b9915e0c596d0d292d70e6652d109
Brian Neal <bgneal@gmail.com>
2025-12-15 19:08:07 -0600

    Add unit tests.
    - Add test for EnigmaSetup
    - Add test for EnigmaModel

--- test/features/enigma/EnigmaModel.test.tsx
+++ test/features/enigma/EnigmaModel.test.tsx
@@ -1,10 +1,54 @@
 import { screen } from "@testing-library/react";
-import { expect, test } from "vitest";
-import EnigmaModel from "../../../src/features/enigma/EnigmaModel.tsx";
+import { userEvent } from "@testing-library/user-event";
+import { describe, expect, test } from "vitest";
+import EnigmaSetup from "../../../src/features/enigma/EnigmaSetup.tsx";
 import { renderWithProviders } from "../../utils/test-utils.tsx";
 
-test("Enigma Model trial test", () => {
-  renderWithProviders(<EnigmaModel />);
+describe("EnigmaModel", () => {
+  test("Radio buttons change the model", async () => {
+    const user = userEvent.setup();
+    renderWithProviders(<EnigmaSetup />);
 
-  expect(screen.getByText("Select Model")).toBeInTheDocument();
+    expect(screen.getByText("Select Model")).toBeInTheDocument();
+
+    let fourRotorOption = screen.getByRole("radio", {
+      name: "Four Rotor (Navy M4)",
+    });
+    expect(fourRotorOption).toBeEnabled();
+    expect(fourRotorOption).not.toBeChecked();
+    await user.click(fourRotorOption);
+
+    const nextButton = screen.getByRole("button", { name: "Next" });
+    expect(nextButton).toBeEnabled();
+    await user.click(nextButton);
+
+    let rotorSelects = screen.getAllByRole("combobox", { name: /Rotor \d/ });
+    expect(rotorSelects).toHaveLength(4);
+
+    const backButton = screen.getByRole("button", { name: "Back" });
+    await user.click(backButton);
+    expect(backButton).toBeDisabled();
+
+    fourRotorOption = screen.getByRole("radio", {
+      name: "Four Rotor (Navy M4)",
+    });
+    expect(fourRotorOption).toBeChecked();
+
+    let threeRotorOption = screen.getByRole("radio", {
+      name: "Three Rotor (Army, Air Force, Navy M3)",
+    });
+    expect(threeRotorOption).toBeEnabled();
+    expect(threeRotorOption).not.toBeChecked();
+    await user.click(threeRotorOption);
+
+    await user.click(nextButton);
+    rotorSelects = screen.getAllByRole("combobox", { name: /Rotor \d/ });
+    expect(rotorSelects).toHaveLength(3);
+
+    await user.click(backButton);
+    threeRotorOption = screen.getByRole("radio", {
+      name: "Three Rotor (Army, Air Force, Navy M3)",
+    });
+    expect(threeRotorOption).toBeChecked();
+  });
 });
