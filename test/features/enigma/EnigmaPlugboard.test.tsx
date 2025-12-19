cfd8a8143dab772badad7cd940d857dd3f42e6cf
Brian Neal <bgneal@gmail.com>
2025-12-18 19:21:03 -0600

    Refactor tests to reduce duplicate code.

--- test/features/enigma/EnigmaPlugboard.test.tsx
+++ test/features/enigma/EnigmaPlugboard.test.tsx
@@ -1,5 +1,5 @@
 import { screen, within } from "@testing-library/react";
-import { userEvent } from "@testing-library/user-event";
+import { type UserEvent, userEvent } from "@testing-library/user-event";
 import { beforeEach, describe, expect, test } from "vitest";
 import { setupStore } from "../../../src/app/store.ts";
 import EnigmaPlugboard from "../../../src/features/enigma/EnigmaPlugboard.tsx";
@@ -11,14 +11,6 @@ import { aCode } from "../../../src/features/enigma/utils.ts";
 import { renderWithProviders } from "../../utils/test-utils.tsx";
 
 describe("EnigmaPlugboard", () => {
-  let numberPlugs: number[];
-  let letterPlugs: string[];
-
-  beforeEach(() => {
-    numberPlugs = Array.from({ length: 26 }).map((_, i) => i + 1);
-    letterPlugs = numberPlugs.map((n) => String.fromCharCode(aCode + n - 1));
-  });
-
   function testDefaultCableCount() {
     // Cable count should default to 10.
     const cableCount = screen.getByRole("combobox", { name: "Cable Count" });
@@ -63,154 +55,136 @@ describe("EnigmaPlugboard", () => {
     expect(letterOption).not.toBeChecked();
   });
 
-  test("Two cable test with the dropdown controls - letters", async () => {
-    const user = userEvent.setup();
-    const store = setupStore();
-    store.dispatch(modelChanged(3));
-    store.dispatch(setupStepChanged(3));
+  describe("Cable tests", () => {
+    let numberPlugs: string[];
+    let letterPlugs: string[];
 
-    renderWithProviders(<EnigmaPlugboard />, { store });
+    beforeEach(() => {
+      numberPlugs = Array.from({ length: 26 }).map((_, i) => `${i + 1}`);
+      letterPlugs = Array.from({ length: 26 }).map((_, i) =>
+        String.fromCharCode(aCode + i),
+      );
+    });
 
-    expect(screen.getByText("Connections (0):")).toBeInTheDocument();
-    expect(screen.getByText("No connections")).toBeInTheDocument();
-    expect(screen.getByRole("radio", { name: "Letters" })).toBeChecked();
+    function setup(numRotors: number) {
+      const store = setupStore();
+      store.dispatch(modelChanged(numRotors));
+      store.dispatch(setupStepChanged(3));
+      return {
+        user: userEvent.setup(),
+        ...renderWithProviders(<EnigmaPlugboard />, { store }),
+      };
+    }
 
-    const plug1 = screen.getByRole("combobox", { name: "Plug 1" });
-    const plug2 = screen.getByRole("combobox", { name: "Plug 2" });
+    async function selectPlug(
+      user: UserEvent,
+      select: HTMLElement,
+      n: number,
+      expectedChoices: string[],
+    ) {
+      await user.click(select);
+      const menuItems = screen.getAllByRole("option");
+      const choices = menuItems.map((m) => m.textContent);
+      expect(choices).toEqual(expectedChoices);
+      await user.click(menuItems[n]);
+    }
 
-    await user.click(plug1);
-    let menuItems = screen.getAllByRole("option");
-    expect(menuItems).toHaveLength(26);
-    let choices = menuItems.map((m) => m.textContent);
-    expect(choices).toEqual(letterPlugs);
-    await user.click(menuItems[12]);
+    test("Two cable test with the dropdown controls - letters", async () => {
+      const { user } = setup(3);
 
-    await user.click(plug2);
-    menuItems = screen.getAllByRole("option");
-    expect(menuItems).toHaveLength(25);
-    choices = menuItems.map((m) => m.textContent);
-    letterPlugs.splice(12, 1);
-    expect(choices).toEqual(letterPlugs);
-    await user.click(menuItems[11]);
+      expect(screen.getByText("Connections (0):")).toBeInTheDocument();
+      expect(screen.getByText("No connections")).toBeInTheDocument();
+      expect(screen.getByRole("radio", { name: "Letters" })).toBeChecked();
 
-    const connectButton = screen.getByRole("button", { name: "Connect" });
-    await user.click(connectButton);
+      const plug1 = screen.getByRole("combobox", { name: "Plug 1" });
+      const plug2 = screen.getByRole("combobox", { name: "Plug 2" });
 
-    expect(screen.getByText("Connections (1):")).toBeInTheDocument();
-    expect(screen.queryByText("No connections")).not.toBeInTheDocument();
-    expect(screen.getByRole("button", { name: "LM" })).toBeInTheDocument();
+      await selectPlug(user, plug1, 12, letterPlugs);
+      letterPlugs.splice(12, 1);
+      await selectPlug(user, plug2, 11, letterPlugs);
 
-    await user.click(plug1);
-    menuItems = screen.getAllByRole("option");
-    expect(menuItems).toHaveLength(24);
-    choices = menuItems.map((m) => m.textContent);
-    letterPlugs.splice(letterPlugs.indexOf("L"), 1);
-    expect(choices).toEqual(letterPlugs);
-    await user.click(menuItems[0]);
+      const connectButton = screen.getByRole("button", { name: "Connect" });
+      await user.click(connectButton);
 
-    await user.click(plug2);
-    menuItems = screen.getAllByRole("option");
-    expect(menuItems).toHaveLength(23);
-    choices = menuItems.map((m) => m.textContent);
-    letterPlugs.splice(letterPlugs.indexOf("A"), 1);
-    expect(choices).toEqual(letterPlugs);
-    await user.click(menuItems[22]);
+      expect(screen.getByText("Connections (1):")).toBeInTheDocument();
+      expect(screen.queryByText("No connections")).not.toBeInTheDocument();
+      expect(screen.getByRole("button", { name: "LM" })).toBeInTheDocument();
 
-    await user.click(connectButton);
-    expect(screen.getByText("Connections (2):")).toBeInTheDocument();
-    expect(screen.queryByText("No connections")).not.toBeInTheDocument();
-    const azButton = screen.getByRole("button", { name: "AZ" });
-    expect(azButton).toBeInTheDocument();
-    expect(screen.getByRole("button", { name: "LM" })).toBeInTheDocument();
+      letterPlugs.splice(letterPlugs.indexOf("L"), 1);
+      await selectPlug(user, plug1, 0, letterPlugs);
 
-    await user.click(within(azButton).getByTestId("CancelIcon"));
-    expect(screen.getByText("Connections (1):")).toBeInTheDocument();
-    expect(screen.queryByText("No connections")).not.toBeInTheDocument();
-    expect(
-      screen.queryByRole("button", { name: "AZ" }),
-    ).not.toBeInTheDocument();
-    expect(screen.getByRole("button", { name: "LM" })).toBeInTheDocument();
+      letterPlugs.splice(letterPlugs.indexOf("A"), 1);
+      await selectPlug(user, plug2, 22, letterPlugs);
+      await user.click(connectButton);
 
-    const clearAll = screen.getByRole("button", { name: "Clear all" });
-    await user.click(clearAll);
+      expect(screen.getByText("Connections (2):")).toBeInTheDocument();
+      expect(screen.queryByText("No connections")).not.toBeInTheDocument();
+      const azButton = screen.getByRole("button", { name: "AZ" });
+      expect(azButton).toBeInTheDocument();
+      expect(screen.getByRole("button", { name: "LM" })).toBeInTheDocument();
 
-    expect(screen.getByText("Connections (0):")).toBeInTheDocument();
-    expect(screen.getByText("No connections")).toBeInTheDocument();
-  });
+      await user.click(within(azButton).getByTestId("CancelIcon"));
+      expect(screen.getByText("Connections (1):")).toBeInTheDocument();
+      expect(screen.queryByText("No connections")).not.toBeInTheDocument();
+      expect(
+        screen.queryByRole("button", { name: "AZ" }),
+      ).not.toBeInTheDocument();
+      expect(screen.getByRole("button", { name: "LM" })).toBeInTheDocument();
 
-  test("Two cable test with the dropdown controls - numbers", async () => {
-    const user = userEvent.setup();
-    const store = setupStore();
-    store.dispatch(modelChanged(3));
-    store.dispatch(setupStepChanged(3));
+      const clearAll = screen.getByRole("button", { name: "Clear all" });
+      await user.click(clearAll);
 
-    renderWithProviders(<EnigmaPlugboard />, { store });
+      expect(screen.getByText("Connections (0):")).toBeInTheDocument();
+      expect(screen.getByText("No connections")).toBeInTheDocument();
+    });
 
-    expect(screen.getByText("Connections (0):")).toBeInTheDocument();
-    expect(screen.getByText("No connections")).toBeInTheDocument();
-    expect(screen.getByRole("radio", { name: "Letters" })).toBeChecked();
-    await user.click(screen.getByRole("radio", { name: "Numbers" }));
+    test("Two cable test with the dropdown controls - numbers", async () => {
+      const { user } = setup(3);
 
-    const plug1 = screen.getByRole("combobox", { name: "Plug 1" });
-    const plug2 = screen.getByRole("combobox", { name: "Plug 2" });
+      expect(screen.getByText("Connections (0):")).toBeInTheDocument();
+      expect(screen.getByText("No connections")).toBeInTheDocument();
+      expect(screen.getByRole("radio", { name: "Letters" })).toBeChecked();
+      await user.click(screen.getByRole("radio", { name: "Numbers" }));
 
-    await user.click(plug1);
-    let menuItems = screen.getAllByRole("option");
-    expect(menuItems).toHaveLength(26);
-    let choices = menuItems.map((m) => parseInt(m.textContent));
-    expect(choices).toEqual(numberPlugs);
-    await user.click(menuItems[12]);
+      const plug1 = screen.getByRole("combobox", { name: "Plug 1" });
+      const plug2 = screen.getByRole("combobox", { name: "Plug 2" });
 
-    await user.click(plug2);
-    menuItems = screen.getAllByRole("option");
-    expect(menuItems).toHaveLength(25);
-    choices = menuItems.map((m) => parseInt(m.textContent));
-    numberPlugs.splice(12, 1);
-    expect(choices).toEqual(numberPlugs);
-    await user.click(menuItems[11]);
+      await selectPlug(user, plug1, 12, numberPlugs);
+      numberPlugs.splice(12, 1);
+      await selectPlug(user, plug2, 11, numberPlugs);
 
-    const connectButton = screen.getByRole("button", { name: "Connect" });
-    await user.click(connectButton);
+      const connectButton = screen.getByRole("button", { name: "Connect" });
+      await user.click(connectButton);
 
-    expect(screen.getByText("Connections (1):")).toBeInTheDocument();
-    expect(screen.queryByText("No connections")).not.toBeInTheDocument();
-    expect(screen.getByRole("button", { name: "12/13" })).toBeInTheDocument();
+      expect(screen.getByText("Connections (1):")).toBeInTheDocument();
+      expect(screen.queryByText("No connections")).not.toBeInTheDocument();
+      expect(screen.getByRole("button", { name: "12/13" })).toBeInTheDocument();
 
-    await user.click(plug1);
-    menuItems = screen.getAllByRole("option");
-    expect(menuItems).toHaveLength(24);
-    choices = menuItems.map((m) => parseInt(m.textContent));
-    numberPlugs.splice(numberPlugs.indexOf(12), 1);
-    expect(choices).toEqual(numberPlugs);
-    await user.click(menuItems[0]);
+      numberPlugs.splice(numberPlugs.indexOf("12"), 1);
+      await selectPlug(user, plug1, 0, numberPlugs);
+      numberPlugs.splice(numberPlugs.indexOf("1"), 1);
+      await selectPlug(user, plug2, 22, numberPlugs);
 
-    await user.click(plug2);
-    menuItems = screen.getAllByRole("option");
-    expect(menuItems).toHaveLength(23);
-    choices = menuItems.map((m) => parseInt(m.textContent));
-    numberPlugs.splice(numberPlugs.indexOf(1), 1);
-    expect(choices).toEqual(numberPlugs);
-    await user.click(menuItems[22]);
+      await user.click(connectButton);
+      expect(screen.getByText("Connections (2):")).toBeInTheDocument();
+      expect(screen.queryByText("No connections")).not.toBeInTheDocument();
+      const azButton = screen.getByRole("button", { name: "1/26" });
+      expect(azButton).toBeInTheDocument();
+      expect(screen.getByRole("button", { name: "12/13" })).toBeInTheDocument();
 
-    await user.click(connectButton);
-    expect(screen.getByText("Connections (2):")).toBeInTheDocument();
-    expect(screen.queryByText("No connections")).not.toBeInTheDocument();
-    const azButton = screen.getByRole("button", { name: "1/26" });
-    expect(azButton).toBeInTheDocument();
-    expect(screen.getByRole("button", { name: "12/13" })).toBeInTheDocument();
+      await user.click(within(azButton).getByTestId("CancelIcon"));
+      expect(screen.getByText("Connections (1):")).toBeInTheDocument();
+      expect(screen.queryByText("No connections")).not.toBeInTheDocument();
+      expect(
+        screen.queryByRole("button", { name: "1/26" }),
+      ).not.toBeInTheDocument();
+      expect(screen.getByRole("button", { name: "12/13" })).toBeInTheDocument();
 
-    await user.click(within(azButton).getByTestId("CancelIcon"));
-    expect(screen.getByText("Connections (1):")).toBeInTheDocument();
-    expect(screen.queryByText("No connections")).not.toBeInTheDocument();
-    expect(
-      screen.queryByRole("button", { name: "1/26" }),
-    ).not.toBeInTheDocument();
-    expect(screen.getByRole("button", { name: "12/13" })).toBeInTheDocument();
+      const clearAll = screen.getByRole("button", { name: "Clear all" });
+      await user.click(clearAll);
 
-    const clearAll = screen.getByRole("button", { name: "Clear all" });
-    await user.click(clearAll);
-
-    expect(screen.getByText("Connections (0):")).toBeInTheDocument();
-    expect(screen.getByText("No connections")).toBeInTheDocument();
+      expect(screen.getByText("Connections (0):")).toBeInTheDocument();
+      expect(screen.getByText("No connections")).toBeInTheDocument();
+    });
   });
 });
