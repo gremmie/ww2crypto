52b986c48da2dedf081b3f7c6357123beb1d05c1
Brian Neal <bgneal@gmail.com>
2026-01-11 20:00:30 -0600

    Add option to overwrite existing setups.
    Also added a close X to the dialog.

--- src/features/enigma/saveConfigDialog.tsx
+++ src/features/enigma/saveConfigDialog.tsx
@@ -1,11 +1,16 @@
-import * as React from "react";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import { Switch } from "@mui/material";
 import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import IconButton from "@mui/material/IconButton";
+import TextField from "@mui/material/TextField";
+import * as React from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks.ts";
 import { configNameSaved, selectConfigNames } from "./enigmaSlice.ts";
 
@@ -17,8 +22,10 @@ export default function SaveConfigDialog(props: SaveConfigDialogProps) {
   const dispatch = useAppDispatch();
   const [open, setOpen] = React.useState(false);
   const [configName, setConfigName] = React.useState("");
+  const [canOverwrite, setCanOverwrite] = React.useState(false);
   const configNames = useAppSelector(selectConfigNames);
   const configExists = configNames.includes(configName);
+  const canSave = configName.length !== 0 && (!configExists || canOverwrite);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -48,6 +55,18 @@ export default function SaveConfigDialog(props: SaveConfigDialogProps) {
       </Button>
       <Dialog fullWidth maxWidth="sm" open={open} onClose={handleClose}>
         <DialogTitle>Save Setup</DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={(theme) => ({
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: theme.palette.grey[500],
+          })}
+        >
+          <CloseOutlinedIcon />
+        </IconButton>
         <DialogContent>
           <DialogContentText>Enter a name for this setup:</DialogContentText>
           <form onSubmit={handleSubmit} id="enigma-save-setup-form">
@@ -62,11 +81,23 @@ export default function SaveConfigDialog(props: SaveConfigDialogProps) {
               variant="filled"
               value={configName}
               onChange={(e) => setConfigName(e.target.value)}
-              error={configExists}
+              error={configExists && !canOverwrite}
               helperText={
                 configExists ? "A setup with this name already exists." : ""
               }
             />
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Switch
+                    size="small"
+                    value={canOverwrite}
+                    onChange={() => setCanOverwrite(!canOverwrite)}
+                  />
+                }
+                label="Overwrite existing setup"
+              />
+            </FormGroup>
           </form>
         </DialogContent>
         <DialogActions>
@@ -77,7 +108,7 @@ export default function SaveConfigDialog(props: SaveConfigDialogProps) {
             variant="contained"
             type="submit"
             form="enigma-save-setup-form"
-            disabled={configName.length === 0 || configExists}
+            disabled={!canSave}
           >
             Save
           </Button>
