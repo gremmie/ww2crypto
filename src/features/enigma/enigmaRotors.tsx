f7db466402e56b3b403ed5388b2aa14f3a6b93d8
Brian Neal <bgneal@gmail.com>
2026-01-26 18:23:46 -0600

    Responsive tweaks for rotor setup on mobile.
    Moved info alerts to help icon w/tooltip.

--- src/features/enigma/enigmaRotors.tsx
+++ src/features/enigma/enigmaRotors.tsx
@@ -1,9 +1,9 @@
-import Alert from "@mui/material/Alert";
-import Box from "@mui/material/Box";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
+import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
-import type { ReactElement } from "react";
+import { type ReactElement } from "react";
 
 import { useAppSelector } from "../../app/hooks.ts";
 import { selectNumberOfRotors } from "./enigmaSlice.ts";
@@ -18,33 +18,38 @@ export default function EnigmaRotors() {
     selects.push(<RotorSelect key={i} rotorNumber={i} />);
   }
 
+  const helpContent = (
+    <>
+      <Typography component="p">
+        There is only one instance of each type of rotor. If you pick a rotor a
+        second time it will be removed from the previously installed position.
+      </Typography>
+      {numberOfRotors == 3 && (
+        <Typography component="p" sx={{ mt: 2 }}>
+          Note that rotors VI - VIII were only available on the Naval Enigmas.
+        </Typography>
+      )}
+    </>
+  );
+
   return (
     <Paper elevation={2} sx={{ px: 4, py: 2 }}>
       <Stack spacing={4} alignItems="center">
-        <Typography variant="h6">
+        <Typography variant="h6" display="flex" alignItems="center" gap={0.5}>
           Select reflector, rotors, and rotor order
+          <Tooltip title={helpContent} arrow enterTouchDelay={350}>
+            <HelpOutlineOutlinedIcon />
+          </Tooltip>
         </Typography>
         <Stack
-          direction={{ sm: "column", md: "row" }}
+          direction={{ xs: "column", md: "row" }}
+          useFlexGap
           spacing={2}
           sx={{ mt: 4 }}
           justifyContent="center"
         >
           {selects}
         </Stack>
-        <Box sx={{ mt: 4, maxWidth: 0.7 }}>
-          <Alert severity="info">
-            There is only one instance of each type of rotor. If you pick a
-            rotor a second time it will be removed from the previously installed
-            position.
-          </Alert>
-          {numberOfRotors == 3 && (
-            <Alert severity="info" sx={{ mt: 2 }}>
-              Note that rotors VI - VIII were only available on the Naval
-              Enigmas.
-            </Alert>
-          )}
-        </Box>
       </Stack>
     </Paper>
   );
