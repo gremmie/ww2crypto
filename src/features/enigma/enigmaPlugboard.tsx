7e77791203b2302bbc89aae8b315320963b5a4ff
Brian Neal <bgneal@gmail.com>
2026-02-04 21:24:56 -0600

    Move plugboard connector to its own component.

--- src/features/enigma/enigmaPlugboard.tsx
+++ src/features/enigma/enigmaPlugboard.tsx
@@ -1,54 +1,23 @@
-import CableIcon from "@mui/icons-material/Cable";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import { Tooltip } from "@mui/material";
-import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import * as React from "react";
 
 import { useAppDispatch, useAppSelector } from "../../app/hooks.ts";
 import {
   type NotationType,
-  plugboardConnected,
   plugboardNotationChanged,
-  selectPlugboard,
-  selectPlugboardCableCount,
   selectPlugboardNotation,
 } from "./enigmaSlice.ts";
 import NotationSelector from "./notationSelector.tsx";
 import PlugboardCableCountSelect from "./plugboardCableCountSelect.tsx";
 import PlugboardConnections from "./plugboardConnections.tsx";
-import PlugboardSelect from "./plugboardSelect.tsx";
+import PlugboardConnector from "./plugboardConnector.tsx";
 import PlugboardTextInput from "./plugboardTextInput.tsx";
 
 export default function EnigmaPlugboard() {
   const dispatch = useAppDispatch();
-  const plugboard = useAppSelector(selectPlugboard);
   const notation = useAppSelector(selectPlugboardNotation);
-  const cableCount = useAppSelector(selectPlugboardCableCount);
-
-  const [first, setFirst] = React.useState<string | null>(null);
-  const [second, setSecond] = React.useState<string | null>(null);
-
-  const connections = plugboard === "" ? [] : plugboard.split(" ");
-  const numberOfConnections = connections.length;
-
-  // Choices already in use.
-  const inUse =
-    numberOfConnections === 0 ? [] : plugboard.replace(" ", "").split("");
-
-  const aCode = "A".charCodeAt(0);
-  const choices1 = Array.from({ length: 26 }, (_, i) =>
-    String.fromCharCode(aCode + i),
-  ).filter((c) => !inUse.includes(c));
-  const choices2 = first == null ? [] : choices1.filter((c) => c !== first);
-
-  const handleConnect = () => {
-    if (first === null || second === null) return;
-    dispatch(plugboardConnected(first + second));
-    setFirst(null);
-    setSecond(null);
-  };
 
   const handleNotationChange = (choice: NotationType) => {
     dispatch(plugboardNotationChanged(choice));
@@ -77,44 +46,7 @@ export default function EnigmaPlugboard() {
         />
       </Stack>
       <PlugboardConnections />
-      <Stack
-        spacing={2}
-        direction="row"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <PlugboardSelect
-          label="Plug 1"
-          notation={notation}
-          disabled={cableCount === connections.length || choices1.length === 0}
-          value={first}
-          choices={choices1}
-          onChange={setFirst}
-        />
-        <CableIcon
-          sx={{
-            display: {
-              xs: "none",
-              sm: "block",
-            },
-          }}
-        />
-        <PlugboardSelect
-          label="Plug 2"
-          notation={notation}
-          disabled={first === null}
-          value={second}
-          choices={choices2}
-          onChange={setSecond}
-        />
-        <Button
-          variant="contained"
-          disabled={first === null || second === null}
-          onClick={handleConnect}
-        >
-          Connect
-        </Button>
-      </Stack>
+      <PlugboardConnector />
       <Typography variant="h6" component="span">
         Or
       </Typography>
---
+++ src/features/enigma/plugboardConnector.tsx
@@ -0,0 +1,83 @@
+import CableIcon from "@mui/icons-material/Cable";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import * as React from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks.ts";
+import {
+  plugboardConnected,
+  selectPlugboard,
+  selectPlugboardCableCount,
+  selectPlugboardNotation,
+} from "./enigmaSlice.ts";
+import PlugboardSelect from "./plugboardSelect.tsx";
+
+export default function PlugboardConnector() {
+  const dispatch = useAppDispatch();
+  const plugboard = useAppSelector(selectPlugboard);
+  const notation = useAppSelector(selectPlugboardNotation);
+  const cableCount = useAppSelector(selectPlugboardCableCount);
+
+  const [first, setFirst] = React.useState<string | null>(null);
+  const [second, setSecond] = React.useState<string | null>(null);
+
+  const connections = plugboard === "" ? [] : plugboard.split(" ");
+  const numberOfConnections = connections.length;
+
+  // Choices already in use.
+  const inUse =
+    numberOfConnections === 0 ? [] : plugboard.replace(" ", "").split("");
+
+  const aCode = "A".charCodeAt(0);
+  const choices1 = Array.from({ length: 26 }, (_, i) =>
+    String.fromCharCode(aCode + i),
+  ).filter((c) => !inUse.includes(c));
+  const choices2 = first == null ? [] : choices1.filter((c) => c !== first);
+
+  const handleConnect = () => {
+    if (first === null || second === null) return;
+    dispatch(plugboardConnected(first + second));
+    setFirst(null);
+    setSecond(null);
+  };
+
+  return (
+    <Stack
+      spacing={2}
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <PlugboardSelect
+        label="Plug 1"
+        notation={notation}
+        disabled={cableCount === connections.length || choices1.length === 0}
+        value={first}
+        choices={choices1}
+        onChange={setFirst}
+      />
+      <CableIcon
+        sx={{
+          display: {
+            xs: "none",
+            sm: "block",
+          },
+        }}
+      />
+      <PlugboardSelect
+        label="Plug 2"
+        notation={notation}
+        disabled={first === null}
+        value={second}
+        choices={choices2}
+        onChange={setSecond}
+      />
+      <Button
+        variant="contained"
+        disabled={first === null || second === null}
+        onClick={handleConnect}
+      >
+        Connect
+      </Button>
+    </Stack>
+  );
+}
