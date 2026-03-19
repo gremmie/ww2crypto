f395e73ed58f4f757144dbc1b576dc3746e85e18
Brian Neal <bgneal@gmail.com>
2026-03-19 18:18:10 -0500

    Move the drum to its own component.

--- src/features/m209/components/setup/drumSetup.tsx
+++ src/features/m209/components/setup/drumSetup.tsx
@@ -1,50 +1,12 @@
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
-import { useState } from "react";
-import { modulo } from "../../../common/utils.ts";
-import DrumBar from "./drumBar.tsx";
-import DrumRollers from "./drumRollers.tsx";
+import Drum from "./drum.tsx";
 
 export default function DrumSetup() {
-  const [bottomBar, setBottomBar] = useState<number>(0);
-  const numBars = 27;
-  const numVisibleBars = 4;
-  const visibleBars = Array.from(
-    { length: numVisibleBars },
-    (_, i) => modulo(bottomBar + numVisibleBars - 1 - i, numBars) + 1,
-  );
-
-  const handleBack = () => {
-    setBottomBar(modulo(bottomBar - 1, numBars));
-  };
-  const handleForward = () => {
-    setBottomBar(modulo(bottomBar + 1, numBars));
-  };
-
   return (
     <Box display="flex" justifyContent="center">
       <Stack direction="column" spacing={3} width={350}>
-        <>
-          <DrumRollers handleBack={handleBack} handleForward={handleForward} />
-          {visibleBars.map((bar) => (
-            <Stack
-              key={bar}
-              direction="row"
-              spacing={2}
-              display="flex"
-              justifyContent="space-between"
-            >
-              <Box width={300}>
-                <DrumBar id={bar} />
-              </Box>
-              <Typography variant="h6" component="div">
-                {bar}
-              </Typography>
-            </Stack>
-          ))}
-          <DrumRollers handleBack={handleBack} handleForward={handleForward} />
-        </>
+        <Drum />
       </Stack>
     </Box>
   );
