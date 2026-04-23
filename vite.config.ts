8acac2a277a56ac0c6760b9392954ea8bf2b0dc6
Brian Neal <bgneal@gmail.com>
2026-04-22 19:21:39 -0500

    Upgrade to Vite 8.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import tanstackRouter from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { configDefaults } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,10 +11,4 @@ export default defineConfig({
     }),
     react(),
   ],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./test/setup.ts",
-    exclude: [...configDefaults.exclude, "e2e/**/*"],
-  },
 });
