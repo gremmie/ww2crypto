88330dd53433b11dc65a19f79f5219ec9e14f2ec
Brian Neal <bgneal@gmail.com>
2026-07-03 22:12:10 -0500

    Fix uncovered errors in e2e test.

--- e2e/enigma-scharnhorst.spec.ts
+++ e2e/enigma-scharnhorst.spec.ts
@@ -1,4 +1,5 @@
-import { expect, Page, test } from "./fixtures.ts";
+import { expect, test } from "./fixtures.ts";
+import type { Page } from "./fixtures.ts";
 
 test.describe("Enigma Scharnhorst decrypt", () => {
   test.beforeEach(async ({ page }) => {
